a192145061e394e2a44e04c383ca5e96f702a97e
Umpoy <iumpoy@yahoo.com>
2018-09-17 16:47:21 -0700

    added dummy data

---
+++ src/data/students.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        student_name: "Barry Allen",
+        student_course: "The Study of Speed",
+        student_grade: 100
+    },
+    {
+        student_name: "Aurther Curry",
+        student_course: "Swim Class",
+        student_grade: 100
+    },
+    {
+        student_name: "Clark Kent",
+        student_course: "Body Building",
+        student_grade: 100
+    },
+    {
+        student_name: "Oliver Queen",
+        student_course: "Archery",
+        student_grade: 100
+    },
+];
