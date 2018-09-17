0bde3fef97221f1c0a87fc697c44a6edda61d62b
Umpoy <iumpoy@yahoo.com>
2018-09-17 16:14:03 -0700

    got React to work

--- src/components/app.js
+++ src/components/app.js
@@ -1,39 +1,9 @@
 import React, { Component } from 'react';
 import '../assets/css/app.css';
 
-import Form from "./form";
-import Header from './header';
-import Table from './table';
-
-
 class App extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            students: [{
-                name: "Ian",
-                course: "Swaggness101",
-                grade: "100"
-            },
-            {
-                name: "Ian",
-                course: "Swaggness102",
-                grade: "100"
-            }],
-        }
-        console.log("props from app: ", this.state)
-    }
     render() {
-        return (
-            <div className="container">
-                <Header />
-                <div className="row">
-                    <Form value={this.state} />
-                    <Table value={this.state} />
-                </div>
-            </div>
-        );
+        return <h1>Hello World</h1>
     }
 }
 
