d4391a693b8c018def761586ea3a56389640fe45
Umpoy <iumpoy@yahoo.com>
2018-09-17 16:59:08 -0700

    now connected with student data

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import StudentList from "./student_list"
 import '../assets/css/app.css';
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
         return (
             <div className="container">
                 <h1 className="text-center">Grade Table</h1>
+                <StudentList />
             </div>
         )
     }
--- src/components/student_list.js
+++ src/components/student_list.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
+import StudentCard from './student_card';
+import studentData from '../data/students';
 
 class StudentList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            students: studentData
+        }
+    }
     render() {
+        console.log("Student Data", this.state.students)
         return <h1>Sometext</h1>
     }
 }
