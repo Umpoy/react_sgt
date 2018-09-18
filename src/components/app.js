c38d1d693646be2903b9804ef98a454db36867b4
Umpoy <iumpoy@yahoo.com>
2018-09-18 14:38:12 -0700

    moved student list constructor to app file

--- src/components/app.js
+++ src/components/app.js
@@ -3,18 +3,27 @@ import StudentList from "./student_list";
 import StudentForm from "./student_form";
 import '../assets/css/app.css';
 
-export default () => {
-    return (
-        <div className="container">
-            <h1 className="text-center my-3">Grade Table</h1>
-            <div className="row">
-                <div className="col-4">
-                    <StudentForm />
-                </div>
-                <StudentList />
+class App extends Component {
+    constructor(props) {
+        super(props);
 
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div className="container" >
+                <h1 className="text-center my-3">Grade Table</h1>
+                <div className="row">
+                    <div className="col-4">
+                        <StudentForm />
+                    </div>
+                    <StudentList />
+
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
+export default App;
--- src/components/student_list.js
+++ src/components/student_list.js
@@ -1,18 +1,10 @@
 import React, { Component } from 'react';
 import StudentCard from './student_card';
-import studentData from '../data/students';
 
 class StudentList extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            students: studentData
-        }
-    }
 
     render() {
-        const list = this.state.students.map((item, index) => {
+        const list = this.props.students.map((item, index) => {
             return <StudentCard key={index} contact={item} />
         });
 
