dfe08b22b3ee2b96374013fd84c25e1bd4d94852
Umpoy <iumpoy@yahoo.com>
2018-09-17 21:39:36 -0700

    input is set to state

--- src/components/student_form.js
+++ src/components/student_form.js
@@ -3,18 +3,35 @@ import React, { Component } from 'react';
 class StudentForm extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            form: {
+                firstName: '',
+                lastName: ''
+            }
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
     }
+
+    handleInputChange(event) {
+        const { value, name } = event.target;
+        const { form } = this.state;
+        form[name] = value;
+        this.setState({
+            form: { ...form }
+        });
+    }
+
     render() {
+        const { firstName, lastName } = this.state.form;
         return (
             <form>
                 <div className="form-group">
                     <label>First Name</label>
-                    <input name="firstName" type="text" className="form-control" />
+                    <input name="firstName" type="text" className="form-control" value={firstName} onChange={this.handleInputChange} />
                 </div>
                 <div className="form-group">
                     <label>Last Name</label>
-                    <input name="lastName" type="text" className="form-control" />
+                    <input name="lastName" type="text" className="form-control" value={lastName} onChange={this.handleInputChange} />
                 </div>
             </form>
         );
