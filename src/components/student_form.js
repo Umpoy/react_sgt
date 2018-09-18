b7cfb34f7dc6aeb98c054e19bd12c58bec479d6b
Umpoy <iumpoy@yahoo.com>
2018-09-18 14:13:01 -0700

    can console log student data on form submit

--- src/components/student_form.js
+++ src/components/student_form.js
@@ -10,6 +10,7 @@ class StudentForm extends Component {
             }
         };
         this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleInputChange(event) {
@@ -21,10 +22,15 @@ class StudentForm extends Component {
         });
     }
 
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log('handleSubmit called, form values are:', this.state.form);
+    }
+
     render() {
         const { firstName, lastName } = this.state.form;
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <div className="form-group">
                     <label>First Name</label>
                     <input name="firstName" type="text" className="form-control" value={firstName} onChange={this.handleInputChange} />
@@ -33,6 +39,7 @@ class StudentForm extends Component {
                     <label>Last Name</label>
                     <input name="lastName" type="text" className="form-control" value={lastName} onChange={this.handleInputChange} />
                 </div>
+                <button>Add Student</button>
             </form>
         );
     }
