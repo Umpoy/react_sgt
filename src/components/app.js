b24ca94975920f21413475bed9b7b6399cb355da
Umpoy <iumpoy@yahoo.com>
2018-09-18 15:07:41 -0700

    bug fix after app constructor made

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import StudentList from "./student_list";
 import StudentForm from "./student_form";
+import studentData from '../data/students';
 import '../assets/css/app.css';
 
 class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {}
+        this.state = {
+            students: studentData
+        }
     }
 
     render() {
@@ -18,7 +21,7 @@ class App extends Component {
                     <div className="col-4">
                         <StudentForm />
                     </div>
-                    <StudentList />
+                    <StudentList students={this.state.students} />
 
                 </div>
             </div>
